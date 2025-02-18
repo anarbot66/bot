bea1215a5b7aac0803e6c26975f350e560a34ad2
anarbot66 <thehoolystar@gmail.com>
2025-02-18 18:01:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,56 +1,19 @@
-// bot.js
-
-// Подключаем dotenv для работы с переменными окружения
 require('dotenv').config(); 
-
-// Подключаем TelegramBot и Express
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 
-// Чтение токена из переменной окружения
 const token = process.env.TELEGRAM_BOT_TOKEN;
-
-// Создание экземпляра бота
 const bot = new TelegramBot(token, { polling: true });
 
-// Создаем Express приложение
 const app = express();
-
-// Указание порта для Render или локально (по умолчанию 3000)
 const PORT = process.env.PORT || 3000;
 
-// Обработка команды /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-
-  // Создаем обычную клавиатуру с кнопкой
-  const keyboard = {
-    keyboard: [
-      [
-        {
-          text: 'Открыть POLE', // Текст кнопки
-          web_app: { url: 'https://pole-cwd8.onrender.com/' } // Ссылка на Mini App
-        }
-      ]
-    ],
-    resize_keyboard: true,
-    one_time_keyboard: false
-  };
-
-  bot.sendMessage(chatId, 'Привет! Я POLE, покажу тебе всю интересную статистику Формулы-1!', {
-    reply_markup: keyboard
-  });
-});
-
-// Добавление новой команды /justanincident
-bot.onText(/\/justanincident/, (msg) => {
-  const chatId = msg.chat.id;
   
-  // Ответ на команду
-  bot.sendMessage(chatId, "nothing special, just an incident. заходи в polе!");
+  bot.sendMessage(chatId, "Привет! Добро пожаловать в POLE!");
 });
 
-// Настроим Express на прослушивание порта
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
