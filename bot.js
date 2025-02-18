1d3b0ed40597ace2223de838d860d3869667559a
anarbot66 <thehoolystar@gmail.com>
2025-02-18 18:02:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 
@@ -8,12 +8,17 @@ const bot = new TelegramBot(token, { polling: true });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Устанавливаем кастомное меню команд
+bot.setMyCommands([
+  { command: "app", description: "Открыть POLE", web_app: { url: "https://pole-cwd8.onrender.com/" } }
+]);
+
+// Обрабатываем команду /start
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  
-  bot.sendMessage(chatId, "Привет! Добро пожаловать в POLE!");
+  bot.sendMessage(msg.chat.id, "Привет! Добро пожаловать в POLE 🚀\n\nИспользуй меню в Telegram, чтобы открыть Mini App.");
 });
 
+// Запускаем сервер Express
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
